b976a778f0249ef407935c786f9bfe10c5f231ea
Timo Hillmann <timo@clipchamp.com>
2017-02-02 09:25:47 +1000

    Changed version number

--- classes/clipchamp.php
+++ classes/clipchamp.php
@@ -11,7 +11,7 @@ if ( ! class_exists('Clipchamp') ) {
 		protected static $writeable_properties = array();
 		protected $modules;
 
-		const VERSION    = '0.5.3';
+		const VERSION    = '1.5.0';
 		const PREFIX     = 'ccb_';
 		const DEBUG_MODE = false;
 
